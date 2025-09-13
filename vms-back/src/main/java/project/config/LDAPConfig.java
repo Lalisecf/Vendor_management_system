7bdb13e2ec6e8b4e2d142b126027e81a80c2bc1c
Lalise <lalisefufi5@gmail.com>
2025-09-13 11:03:40 +0300

    Update LDAPConfig.java
    s

--- vms-back/src/main/java/project/config/LDAPConfig.java
+++ vms-back/src/main/java/project/config/LDAPConfig.java
@@ -10,18 +10,11 @@ public class LDAPConfig {
     @Bean
     public LdapTemplate ldapTemplate() {
         LdapContextSource contextSource = new LdapContextSource();
-        // for production server
 
-        // contextSource.setUrl("ldap://10.10.12.2:389");
-        // contextSource.setBase("dc=awash,dc=local");
-        // contextSource.setUserDn("demekeaya@awash.local");
-        // contextSource.setPassword("Final@2233");
 
         // for test server
-        contextSource.setUrl("ldap://awashtest.local");
-        contextSource.setBase("dc=awashtest,dc=local");
-        // contextSource.setUserDn("demekea@awashtest.local");
-        // contextSource.setPassword("dem@aib22");
+        contextSource.setUrl("ld");
+        contextSource.setBase("d");
         contextSource.afterPropertiesSet(); // required
         return new LdapTemplate(contextSource);
     }
